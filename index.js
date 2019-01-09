922215e1406cfe003d27e6e46ab6d0fca59774a3
William Saffran <willh.saffran@gmail.com>
2019-01-09 17:07:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-function theBeatlesPlay(musicians, instruments) {
-  var array = []
-  for (let i = 0; i < musicians.length; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`)
-  }
-  return array
-}
-
-function johnLennonFacts(facts) {
-  var i = 0
-  while (i < facts.length) {
-    facts[i] = `${facts[i]}!!!`
-  }
-  return facts
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var newArray = []
@@ -57,7 +27,6 @@ function johnLennonFacts(array) {
   }
   return newArray
 }*/
-
 
 function iLoveTheBeatles(number) {
   var newArray = []
